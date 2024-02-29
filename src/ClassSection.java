32082c041fef90853f545548aeaeda3bf4e17d03
meaganhsu <myth72@student.cis.edu.hk>
2024-02-29 12:39:15 +0800

    done

--- src/ClassSection.java
+++ src/ClassSection.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
 
 public class ClassSection {
-
-
+    private String subject;
+    private int capacity;
+    private int yearLevel;
+    private ArrayList<Student> students;
+    public ClassSection(String subject, int capacity, int yearLevel) {
+        this.subject = subject;
+        this.capacity = capacity;
+        this.yearLevel = yearLevel;
+        students = new ArrayList<>();
+    }
+    public String getSubject() {return subject;}
+    public void setSubject(String x) {subject = x;}
+    public int getCapacity() {return capacity;}
+    public void setCapacity(int x) {capacity = x;}
+    public int getYearLevel() {return yearLevel;}
+    public void setYearLevel(int x) {yearLevel = x;}
+    public ArrayList<Student> getStudents() {return students;}
+    public void addStudent(Student s) {
+        if (!students.contains(s) && s.getYearLevel() == yearLevel) students.add(s);
+    }
+    public void removeStudent(Student s) {students.remove(s);}
+    public boolean isStudentEnrolled(Student s) {return students.contains(s);}
+    public String toString() {
+        return "ClassSection{subject='" + subject + "', capacity=" + capacity + ", yearLevel=" + yearLevel + ", students=" + students + "}";
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,31 @@
 public class Student {
-
-
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int yearLevel;
+    private static int studentCount;
+    public Student(String firstName, String lastName, int age, int yearLevel) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.yearLevel = yearLevel;
+    }
+    public String getFirstName() {return firstName;}
+    public void setFirstName(String x) {firstName = x;}
+    public String getLastName() {return lastName;}
+    public void setLastName(String x) {this.lastName = x;}
+    public int getAge() {return age;}
+    public void setAge(int x) {age = x;}
+    public int getYearLevel() {return yearLevel;}
+    public void setYearLevel(int x) {yearLevel = x;}
+    public int getStudentCount() {return studentCount;}
+    public boolean equals(Student student) {
+        Student x = new Student(firstName, lastName, age, yearLevel);
+        if (x.equals(student)) return true;
+        return false;
+    }
+    public String toString() {
+        return "Student{id=" + id + ", firstName='" + firstName + "', lastName='" + lastName + "', age=" + age + ", yearLevel=" + yearLevel + "}";
+    }
 }
